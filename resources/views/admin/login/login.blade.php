a7c8c00f503a42e2500251475136e125304fe418
Krisna-Putra-Immanuel <krisnaimmanuel12@gmail.com>
2025-04-09 20:42:25 +0700

    login

--- resources/views/admin/login/login.blade.php
+++ resources/views/admin/login/login.blade.php
@@ -31,15 +31,15 @@
     </div>
   </div>
   <main class="main-content  mt-0">
-    <div class="page-header align-items-start min-vh-100" style="background-image: url('https://images.unsplash.com/photo-1497294815431-9365093b7331?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80');">
-      <span class="mask bg-gradient-dark opacity-6"></span>
-      <div class="container my-auto">
-        <div class="row">
-          <div class="col-lg-4 col-md-8 col-12 mx-auto">
-            <div class="card z-index-0 fadeIn3 fadeInBottom">
-              <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-                <div class="bg-gradient-dark shadow-dark border-radius-lg py-3 pe-1">
-                  <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">Sign in</h4>
+    <div class="page-header min-vh-100 d-flex align-items-center justify-content-center" style="background-image: url('https://images.unsplash.com/photo-1497294815431-9365093b7331?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80');">
+      <span class="mask bg-gradient-dark opacity-6 "></span>
+      <div class="container position-relative z-index-2">
+      <div class="row justify-content-center">
+        <div class="col-lg-4 col-md-8 col-12">
+          <div class="card z-index-0 fadeIn3 fadeInBottom">
+            <div class="card-header p-0">
+              <div class="bg-gradient-dark shadow-dark border-radius-lg py-3 text-center">
+                <h4 class="text-white font-weight-bolder mb-0">Sign In</h4>
                   <div class="row mt-3">
                     <div class="col-2 text-center ms-auto">
                       <a class="btn btn-link px-3" href="javascript:;">
